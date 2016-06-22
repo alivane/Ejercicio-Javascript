d09e7fa2c9ce9c95409ae96edf8661b7a3b614b9
AliVane <vaneg643@gmail.com>
2016-06-22 10:58:06 -0400

    insertado de ejercicios

--- js/funtion.js
+++ js/funtion.js
@@ -72,7 +72,8 @@ function mayusMin(cadena){
 	}
 	return resultado ; 
 }
-mayusMin ("ALISSON");*/
+cadena=prompt("INGRESA LA PALABRA");
+mayusMin (cadena);*/
 
 
 
@@ -99,13 +100,104 @@ document.write(palindromo(palabra));
 
 
 //EJERCICIO 6
+/*
+function operacion(a,b){
+	var resultado = 0;
+	if(operar == "multiplicacion"){
+		resultado =alert( a*b);
+	}else if(operar =="division"){
+		resultado = alert(a/b);
+	}else if(operar =="resta"){
+		resultado = alert(a-b);
+	}else if(operar =="suma"){
+		resultado = alert(a+b);
+	}
+	return (resultado); 
+}
+var operar = prompt("ingrese la operacion que dese hacer","multiplicacion,division,resta,suma");
+a = parseInt(prompt("Ingrese el numero "));
+b = parseInt(prompt("Ingrese el numero "));
+alert(operacion(a,b));
+*/
 
 
 
 
 
 
+//EJERCICIO 7
+/*
+function cuadrados(a,b){ 
+	var numero = a+b;
+	var cuadrado = numero*numero ;
+	return cuadrado; 
+}
+a = parseInt(prompt("Ingrese el numero "));
+b = parseInt(prompt("Ingrese el numero "));
+alert(cuadrados(a,b));
+*/
 
+
+
+
+//EJERCICIO 8
+
+/*
+function numPrimo(a){
+	for(var i = 2 ; i<a ; i++){
+		if(a*1==a)
+	}	
+
+}
+a = parseInt(prompt("Ingrese el numero "));
+numPrimo(a);
+
+var primo;
+	primo = true ; 
+	for(var i = 2 ; i<a ; i++){
+		if(a%i==0){
+			primo = false ; 
+		}else{
+			primo = true;
+		} 
+	}
+	 return (document.write(primo));
+*/
+
+
+
+
+
+
+//EJERCICIO 9
+/*
+function multiplo(a){
+	var resultado;
+	for(var i = 0 ; i<100 ; i++){
+		if(i%a ==0){
+			resultado = document.write(i+" es multiplo de "+a+"<br>");
+		}
+	}
+	return resultado;
+}
+a = parseInt(prompt("Ingrese el numero"));
+multiplo(a);
+*/
+
+
+
+
+
+//EJERCICIO 10
+
+
+function cantidad(numero){
+	var resultado ;
+	resultado = numero.split(",");
+	return document.write(resultado);
+}
+numero = parseInt(prompt("ingrese los numero"));
+cantidad(numero);
 
 
 
