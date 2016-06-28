ea14912f7fa27d8328ffb775701e6730667d4bc8
AliVane <vaneg643@gmail.com>
2016-06-28 10:26:25 -0400

    insertado de ejercicio 2 de object

---
+++ js/object.js
@@ -0,0 +1,66 @@
+//EJERCICIO 1   informacion Factura
+/*var factura = {
+	empresa  :  {
+		nombre : "Alivane",
+		direccion : "maria graham ",
+		telefono : 2222222,
+		rut : 2222222
+	} , 
+	cliente : {
+		nombre : "Alivane",
+		direccion : "maria graham ",
+		telefono : 2222222,
+		rut : 2222222
+	},
+	elementos : [
+		{precio:10 , cantidad:20 , descripcion:"Autos rojos"},
+		{precio:7 , cantidad:30 , descripcion:"Autos rojos"},
+		{precio:23 , cantidad:15 , descripcion:"Autos rojos"},
+		{precio:2 , cantidad:22 , descripcion:"Autos rojos"}
+	],
+	importeTotal : " ",
+	tipoIva : 1.19 , 
+	formaPago : "Efectivo"
+}
+factura.importe = function(){
+	var total = 0 ;
+	for(var i = 0 ; i<this.elementos.length ; i++){
+		total = total+(this.elementos[i].precio*this.elementos[i].precio);
+	}
+	this.importe = total*1.19 ; 
+}
+factura.mostrar = function(){
+	alert(importe);
+}
+*/
+//EJERCICIO 2
+/*
+var monthActual = {
+	firstCuatrimes : ["January","February","March","April"],
+ 	secondCuatrimes : ["May","Juni","july","August"],
+ 	thirdCuatrimes	 : ["september","Octubre","November","December"],
+ 	current : function(){
+ 		var total;
+ 		for(var i = 0 ; i<this.firstCuatrimes.length ; i++){
+ 			if(month==this.firstCuatrimes[i]){
+ 				total = alert("Estamos en el cuatrimes 1");
+ 			}
+ 		}
+ 		for(var i = 0 ; i<this.secondCuatrimes.length ; i++){
+ 			if(month==this.secondCuatrimes[i]){
+ 				total = alert("Estamos en el cuatrimes 2");
+ 			}
+ 		}
+ 		for(var i = 0 ; i<this.thirdCuatrimes.length ; i++){
+ 			if(month==this.thirdCuatrimes[i]){
+ 				total = alert("Estamos en el cuatrimes 3");
+ 			}
+ 		}
+ 		return total ; 
+ 	}
+}
+var month = prompt("Ingrese el mes actual","January,February,March,April ...");
+monthActual.current();
+
+*/
+
